aa1c00ee40365881c9191c5cc7b6ecb40aa77bd4
vaibhav.bandhan.ai.2005@gmail.com <vaibhav.bandhan.ai.2005@gmail.com>
2026-04-08 19:33:00 +0530

    api change1

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,5 +1,5 @@
 (function () {
-    const api_url = "http://localhost:3000/api/chat";
+    const api_url = "https://shaya-ai.vercel.app/api/chat";
     const scriptTag = document.currentScript;
     const ownerId = scriptTag.getAttribute("data-owner-id") || "default-id";
 
